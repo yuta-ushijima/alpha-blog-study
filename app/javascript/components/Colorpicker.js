618607b35fe1e7b5008f9953e6ceef1e9a9b18d2
yuta-ushijima <register@yuta-u.com>
2018-06-09 17:56:50 +0900

    カラーピッカーで選択された処理が非同期で背景色が変更されるように実装

--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -5,15 +5,27 @@ import { SketchPicker } from "react-color"
 
 class Colorpicker extends React.Component {
 
-  constructor() {
+  constructor(props) {
     /*superは親クラスであるReact.Componentを指す */
     super(props);
+    // this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange = {}
+  handleChange = (color) => {
+    /*カラーピッカーで選択された値が16進数でconsoleに表示される */
+    // console.log(color.hex);
+
+    /*document内のbodyタグのstyleのグローバルスコープで
+    背景色をカラーピッカーで指定された色に変更する。
+    つまり、以下を指定することで、カラーピッカーに選んだ値が非同期ですぐに反映される。 */
+    document.body.style.background = color.hex;
+  }
+
   render() {
     return <React.Fragment>
-      <SketchPicker />
+      {/* colorには、フォームから指定されたカラーコードの値を取得 */}
+      <SketchPicker color={this.props.color}
+        onChange={(event) => this.handleChange(event)} />
     </React.Fragment>
   }
 }
