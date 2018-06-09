446c8cb8b8239f800d8a351a7fc379c5c3db8299
yuta-ushijima <register@yuta-u.com>
2018-06-09 21:53:11 +0900

    Colorpickerコンポーネントの完成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "img-circle")
   end
+
+  def prefered_color
+    # return => ログインしているユーザーのcolorの値をreturnで返す。
+    # if => 現在ログインしているユーザーかつ現在ログインしているユーザーが持っているcolorの値をpresent?メソッドで存在確認
+    return current_user.color if (current_user && current_user.color.present?)
+    '#fff' #デフォルトのカラーは白。ログアウトするとこのカラーが表示される。
+  end
 end
--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -8,7 +8,8 @@ class Colorpicker extends React.Component {
   constructor(props) {
     /*superは親クラスであるReact.Componentを指す */
     super(props);
-    // this.handleChange = this.handleChange.bind(this);
+    // イベントハンドラのbindは右の書き方でもOK。this.handleChange = this.handleChange.bind(this);
+    this.state = { selector: props.selector };
   }
 
   handleChange = (color) => {
@@ -19,12 +20,17 @@ class Colorpicker extends React.Component {
     背景色をカラーピッカーで指定された色に変更する。
     つまり、以下を指定することで、カラーピッカーに選んだ値が非同期ですぐに反映される。 */
     document.body.style.background = color.hex;
+    /*querySelectorは引数に与えたセレクタを参照することができるメソッド */
+    let elem = document.querySelector('#' + this.state.selector);
+    /*valueはカラーピッカーから渡されるカラーコードを指している。 */
+    elem.value = color.hex;
   }
 
   render() {
     return <React.Fragment>
       {/* colorには、フォームから指定されたカラーコードの値を取得 */}
       <SketchPicker color={this.props.color}
+        /*以下の書き方でもイベントハンドラをbindできる(ES6のアロー関数でbind)*/
         onChange={(event) => this.handleChange(event)} />
     </React.Fragment>
   }
